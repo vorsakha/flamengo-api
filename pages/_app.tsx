27e0293424f532a7d681954dd9d301852606cdc2
vorsakha <andreitf.dev@gmail.com>
2021-07-28 17:01:55 -0300

    fixed pre not wrapping text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,6 +40,13 @@ pre {
   border: 1px solid rgba(75, 85, 99, 0.1); 
   border-radius: 4px;
   padding: 1rem;
+
+  // Wrap text that don't respect width
+  white-space: pre-wrap;       /* Since CSS 2.1 */
+    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+    white-space: -pre-wrap;      /* Opera 4-6 */
+    white-space: -o-pre-wrap;    /* Opera 7 */
+    word-wrap: break-word;      /* Internet Explorer 5.5+ */
 }
 
 a {
