a2145c37b25a70e8924a200cd65d5c286dfce4b1
vorsakha <andreitf.dev@gmail.com>
2021-07-29 09:53:35 -0300

    test switch method

--- pages/api/squad.ts
+++ pages/api/squad.ts
@@ -38,95 +38,105 @@ type ElementTypes = {
 const url = "https://www.flamengo.com.br/elencos/elenco-profissional";
 
 const data = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    // Puppeteer
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  const { method } = req;
 
-    await page.goto(url);
+  switch (method) {
+    case "GET":
+      try {
+        // Puppeteer
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-    const data = await page.evaluate(() => document.body.innerHTML);
+        await page.goto(url);
 
-    await browser.close();
-    //
+        const data = await page.evaluate(() => document.body.innerHTML);
 
-    // Parse html text to dom
-    const dom = parse(data);
+        await browser.close();
+        //
 
-    // Manipulate dom
+        // Parse html text to dom
+        const dom = parse(data);
 
-    const players = dom
-      .querySelectorAll(".container")[2]
-      .querySelectorAll(".col-12");
-    const keepers = players[0].querySelectorAll("figure");
-    const centerBacks = players[1].querySelectorAll("figure");
-    const rightBacks = players[2].querySelectorAll("figure");
-    const leftBacks = players[3].querySelectorAll("figure");
-    const midfielders = players[4].querySelectorAll("figure");
-    const attackMidfielders = players[5].querySelectorAll("figure");
-    const strikers = players[6].querySelectorAll("figure");
+        // Manipulate dom
 
-    const obj: ElementTypes = {
-      players: {
-        keepers: [],
-        centerBacks: [],
-        rightBacks: [],
-        leftBacks: [],
-        midfielders: [],
-        attackMidfielders: [],
-        strikers: [],
-      },
-    };
+        const players = dom
+          .querySelectorAll(".container")[2]
+          .querySelectorAll(".col-12");
+        const keepers = players[0].querySelectorAll("figure");
+        const centerBacks = players[1].querySelectorAll("figure");
+        const rightBacks = players[2].querySelectorAll("figure");
+        const leftBacks = players[3].querySelectorAll("figure");
+        const midfielders = players[4].querySelectorAll("figure");
+        const attackMidfielders = players[5].querySelectorAll("figure");
+        const strikers = players[6].querySelectorAll("figure");
 
-    for (let i = 0; i < keepers.length; i++) {
-      obj.players.keepers.push({
-        name: keepers[i].querySelector("p").text,
-        image: keepers[i].querySelector("img")._rawAttrs.src,
-      });
-    }
-    for (let i = 0; i < centerBacks.length; i++) {
-      obj.players.centerBacks.push({
-        name: centerBacks[i].querySelector("p").text,
-        image: centerBacks[i].querySelector("img")._rawAttrs.src,
-      });
-    }
-    for (let i = 0; i < rightBacks.length; i++) {
-      obj.players.rightBacks.push({
-        name: rightBacks[i].querySelector("p").text,
-        image: rightBacks[i].querySelector("img")._rawAttrs.src,
-      });
-    }
-    for (let i = 0; i < leftBacks.length; i++) {
-      obj.players.leftBacks.push({
-        name: leftBacks[i].querySelector("p").text,
-        image: leftBacks[i].querySelector("img")._rawAttrs.src,
-      });
-    }
-    for (let i = 0; i < midfielders.length; i++) {
-      obj.players.midfielders.push({
-        name: midfielders[i].querySelector("p").text,
-        image: midfielders[i].querySelector("img")._rawAttrs.src,
-      });
-    }
-    for (let i = 0; i < attackMidfielders.length; i++) {
-      obj.players.attackMidfielders.push({
-        name: attackMidfielders[i].querySelector("p").text,
-        image: attackMidfielders[i].querySelector("img")._rawAttrs.src,
-      });
-    }
-    for (let i = 0; i < strikers.length; i++) {
-      obj.players.strikers.push({
-        name: strikers[i].querySelector("p").text,
-        image: strikers[i].querySelector("img")._rawAttrs.src,
-      });
-    }
+        const obj: ElementTypes = {
+          players: {
+            keepers: [],
+            centerBacks: [],
+            rightBacks: [],
+            leftBacks: [],
+            midfielders: [],
+            attackMidfielders: [],
+            strikers: [],
+          },
+        };
 
-    // res.setHeader("Cache-Control", "s-maxage=100, stale-while-revalidate");
+        for (let i = 0; i < keepers.length; i++) {
+          obj.players.keepers.push({
+            name: keepers[i].querySelector("p").text,
+            image: keepers[i].querySelector("img")._rawAttrs.src,
+          });
+        }
+        for (let i = 0; i < centerBacks.length; i++) {
+          obj.players.centerBacks.push({
+            name: centerBacks[i].querySelector("p").text,
+            image: centerBacks[i].querySelector("img")._rawAttrs.src,
+          });
+        }
+        for (let i = 0; i < rightBacks.length; i++) {
+          obj.players.rightBacks.push({
+            name: rightBacks[i].querySelector("p").text,
+            image: rightBacks[i].querySelector("img")._rawAttrs.src,
+          });
+        }
+        for (let i = 0; i < leftBacks.length; i++) {
+          obj.players.leftBacks.push({
+            name: leftBacks[i].querySelector("p").text,
+            image: leftBacks[i].querySelector("img")._rawAttrs.src,
+          });
+        }
+        for (let i = 0; i < midfielders.length; i++) {
+          obj.players.midfielders.push({
+            name: midfielders[i].querySelector("p").text,
+            image: midfielders[i].querySelector("img")._rawAttrs.src,
+          });
+        }
+        for (let i = 0; i < attackMidfielders.length; i++) {
+          obj.players.attackMidfielders.push({
+            name: attackMidfielders[i].querySelector("p").text,
+            image: attackMidfielders[i].querySelector("img")._rawAttrs.src,
+          });
+        }
+        for (let i = 0; i < strikers.length; i++) {
+          obj.players.strikers.push({
+            name: strikers[i].querySelector("p").text,
+            image: strikers[i].querySelector("img")._rawAttrs.src,
+          });
+        }
 
-    res.status(200).json(obj);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ error: "Server Error." });
+        res.setHeader("Cache-Control", "s-maxage=100, stale-while-revalidate");
+
+        res.status(200).json(obj);
+      } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: "Server Error." });
+      }
+      break;
+
+    default:
+      res.status(400).json({ error: "Wrong Method." });
+      break;
   }
 };
 
