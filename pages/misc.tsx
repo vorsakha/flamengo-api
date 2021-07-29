7ecb26eff103f35c0c82e719e2f51226e9c0b2de
vorsakha <andreitf.dev@gmail.com>
2021-07-29 18:09:24 -0300

    cors

--- pages/misc.tsx
+++ pages/misc.tsx
@@ -5,8 +5,7 @@ import { InferGetStaticPropsType } from "next";
 
 export async function getStaticProps() {
   try {
-    // const res = await axios.get(`${process.env.VERCEL_URL}/api/misc`);
-    const res = await axios.get(`https://dog.ceo/api/breeds/image/random`);
+    const res = await axios.get(`https://flamengo-api.vercel.app/api/misc`);
     const miscData = res.data;
 
     return {
