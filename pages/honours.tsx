7bea50c37516d493e2777541e9fe5cc2bf0312fe
vorsakha <andreitf.dev@gmail.com>
2021-07-29 14:52:15 -0300

    getStaticProps production

--- pages/honours.tsx
+++ pages/honours.tsx
@@ -3,50 +3,48 @@ import { InferGetStaticPropsType } from "next";
 import Container from "../components/common/container";
 import axios from "axios";
 
-// export async function getStaticProps() {
-//   try {
-//     const res = await axios.get(`${process.env.VERCEL_URL}/api/honours`);
-//     const honoursData = res.data;
+export async function getStaticProps() {
+  try {
+    const res = await axios.get(`${process.env.VERCEL_URL}/api/honours`);
+    const honoursData = res.data;
 
-//     return {
-//       props: {
-//         honours: honoursData,
-//       },
-//     };
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+    return {
+      props: {
+        honours: honoursData,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const Honours = () =>
-  //   {
-  //   honours,
-  // }: InferGetStaticPropsType<typeof getStaticProps>
-  {
-    const json = "honours";
+const Honours = ({
+  honours,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const json = honours;
 
-    return (
-      <Container>
-        <Head>
-          <title>Flamengo API</title>
-          <meta name="description" content="Flamengo football team info" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+  return (
+    <Container>
+      <Head>
+        <title>Flamengo API</title>
+        <meta name="description" content="Flamengo football team info" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <h1>Honours</h1>
-        <code>GET https://flamengo-api.vercel.app/api/honours</code>
+      <h1>Honours</h1>
+      <code>GET https://flamengo-api.vercel.app/api/honours</code>
 
-        <h2 style={{ margin: "2rem 0 1rem 0" }}>Data</h2>
-        <p>
-          <ul>
-            <li>All club professional honours</li>
-          </ul>
-        </p>
+      <h2 style={{ margin: "2rem 0 1rem 0" }}>Data</h2>
+      <p>
+        <ul>
+          <li>All club professional honours</li>
+        </ul>
+      </p>
 
-        <h2 style={{ margin: "2rem 0 1rem 0" }}>Example Response</h2>
-        <pre>{JSON.stringify(json, undefined, 2)}</pre>
-      </Container>
-    );
-  };
+      <h2 style={{ margin: "2rem 0 1rem 0" }}>Example Response</h2>
+      <pre>{JSON.stringify(json, undefined, 2)}</pre>
+    </Container>
+  );
+};
 
 export default Honours;
--- pages/staff.tsx
+++ pages/staff.tsx
@@ -3,48 +3,46 @@ import axios from "axios";
 import Container from "../components/common/container";
 import { InferGetStaticPropsType } from "next";
 
-// export async function getStaticProps() {
-//   try {
-//     const res = await axios.get(`${process.env.VERCEL_URL}/api/staff`);
-//     const staffData = res.data;
+export async function getStaticProps() {
+  try {
+    const res = await axios.get(`${process.env.VERCEL_URL}/api/staff`);
+    const staffData = res.data;
 
-//     return {
-//       props: {
-//         staff: staffData,
-//       },
-//     };
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+    return {
+      props: {
+        staff: staffData,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const Staff = () =>
-  //{ staff }: InferGetStaticPropsType<typeof getStaticProps>
-  {
-    const json = "staff";
+const Staff = ({ staff }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const json = staff;
 
-    return (
-      <Container>
-        <Head>
-          <title>Flamengo API</title>
-          <meta name="description" content="Flamengo football team info" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+  return (
+    <Container>
+      <Head>
+        <title>Flamengo API</title>
+        <meta name="description" content="Flamengo football team info" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <h1>Staff Information</h1>
-        <code>GET https://flamengo-api.vercel.app/api/staff</code>
+      <h1>Staff Information</h1>
+      <code>GET https://flamengo-api.vercel.app/api/staff</code>
 
-        <h2 style={{ margin: "2rem 0 1rem 0" }}>Data</h2>
-        <p>
-          <ul>
-            <li>Professional football staff</li>
-          </ul>
-        </p>
+      <h2 style={{ margin: "2rem 0 1rem 0" }}>Data</h2>
+      <p>
+        <ul>
+          <li>Professional football staff</li>
+        </ul>
+      </p>
 
-        <h2 style={{ margin: "2rem 0 1rem 0" }}>Example Response</h2>
-        <pre>{JSON.stringify(json, undefined, 2)}</pre>
-      </Container>
-    );
-  };
+      <h2 style={{ margin: "2rem 0 1rem 0" }}>Example Response</h2>
+      <pre>{JSON.stringify(json, undefined, 2)}</pre>
+    </Container>
+  );
+};
 
 export default Staff;
