637760a686d80bc33797fd09633af87b3810ff9d
vorsakha <andreitf.dev@gmail.com>
2021-07-30 13:59:06 -0300

    box border on hover

--- components/common/box.tsx
+++ components/common/box.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Box = styled.div`
   border: 1px solid rgba(75, 85, 99, 0.1);
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-  padding: 1rem;
+  padding: 1.5rem;
   background-color: #fcfcfc;
   cursor: pointer;
   border-radius: 4px;
@@ -11,14 +11,17 @@ const Box = styled.div`
   p {
     color: #858585;
     font-weight: 300;
+    margin: 0;
   }
 
   h2 {
     font-size: 1.2rem;
+    margin: 0;
   }
 
   :hover {
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    border: 1px solid rgba(255, 54, 54, 0.5);
   }
 `;
 
